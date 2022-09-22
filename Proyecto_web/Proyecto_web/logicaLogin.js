8f0c03ae98731eeaf13e8f44c63a9dabd7a4a34f
jhonner06 <jdmoreno2338@misena.edu.co>
2022-09-21 23:51:32 -0500

    Se añaden vistas

---
+++ Proyecto_web/Proyecto_web/logicaLogin.js
@@ -0,0 +1,10 @@
+function mostrarPassword(obj){
+    var obj= document.getElementById("password");
+    obj.type="text";
+}
+
+function ocultarPassword(obj){
+    var obj= document.getElementById("password");
+    obj.type="password"
+}
+
